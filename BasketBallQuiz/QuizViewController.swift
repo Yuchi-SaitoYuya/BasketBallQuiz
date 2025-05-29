617e24c51dae205b3754bab107ab278d0c73ee19
Yuchinante <ysuu45143745@gmail.com>
2025-05-29 16:48:25 +0900

    Day1

---
+++ BasketBallQuiz/QuizViewController.swift
@@ -0,0 +1,96 @@
+//
+//  QuizViewController.swift
+//  BasketBallQuiz
+//  
+//  Created by Yuchinante on 2025/05/29
+//  
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+    @IBOutlet var quizNumberLabel: UILabel!
+    @IBOutlet var quizTextView: UITextView!
+    @IBOutlet var answerButton1: UIButton!
+    @IBOutlet var answerButton2: UIButton!
+    @IBOutlet var answerButton3: UIButton!
+    @IBOutlet var answerButton4: UIButton!
+    
+    var csvArray: [String] = []
+    var quizArray: [String] = []
+    var quizCount = 0
+    var correctCount = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        csvArray = loadCSV(fileName: "quiz")
+        print(csvArray)
+
+        quizArray = csvArray[quizCount].components(separatedBy: ",")
+
+        quizNumberLabel.text = "第\(quizCount + 1)問"
+        quizTextView.text = quizArray[0]
+        answerButton1.setTitle(quizArray[2], for: .normal)
+        answerButton2.setTitle(quizArray[3], for: .normal)
+        answerButton3.setTitle(quizArray[4], for: .normal)
+        answerButton4.setTitle(quizArray[5], for: .normal)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let scoreVC = segue.destination as! ScoreViewController
+        scoreVC.correct = correctCount
+    }
+
+    @IBAction func btnAction(sender: UIButton) {
+        if sender.tag == Int(quizArray[1]) {
+            print("正解")
+            correctCount += 1
+        } else {
+            print("不正解")
+        }
+        print("スコア：\(correctCount)")
+        nextQuiz()
+    }
+
+    func nextQuiz() {
+        quizCount += 1
+        if quizCount < csvArray.count {
+            quizArray = csvArray[quizCount].components(separatedBy: ",")
+            quizNumberLabel.text = "第\(quizCount + 1)問"
+            quizTextView.text = quizArray[0]
+            answerButton1.setTitle(quizArray[2], for: .normal)
+            answerButton2.setTitle(quizArray[3], for: .normal)
+            answerButton3.setTitle(quizArray[4], for: .normal)
+            answerButton4.setTitle(quizArray[5], for: .normal)
+        } else {
+            performSegue(withIdentifier: "toScoreVC", sender: nil)
+        }
+    }
+
+    func loadCSV(fileName: String) -> [String] {
+        let csvBundle = Bundle.main.path(forResource: fileName, ofType: "csv")!
+        do {
+            let csvData = try String(contentsOfFile: csvBundle, encoding: String.Encoding.utf8)
+            let lineChange = csvData.replacingOccurrences(of: "\r", with: "\n")
+            csvArray = lineChange.components(separatedBy: "\n")
+            csvArray.removeLast()
+        } catch {
+            print("エラー")
+        }
+        return csvArray
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
